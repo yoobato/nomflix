56b518823734f208e19bdba3e1dc52f48b8a3ba3
Daeyeol Ryu <dy@yoobato.com>
2019-11-21 21:13:45 +0900

    Use styled-components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header``;
+
+const List = styled.ul`
+    display: flex;
+    &:hover {
+        background-color: blue;
+    }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 
 export default () => (
-    <header>
-        <ul>
-            <li>
-                <Link to="/">Movies</Link>
-            </li>
-            <li>
-                <Link to="/tv">TV</Link>
-            </li>
-            <li>
-                <Link to="/search">Search</Link>
-            </li>
-        </ul>
-    </header>
+    <Header>
+        <List>
+            <Item>
+                <SLink to="/">Movies</SLink>
+            </Item>
+            <Item>
+                <SLink to="/tv">TV</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">Search</SLink>
+            </Item>
+        </List>
+    </Header>
 );
